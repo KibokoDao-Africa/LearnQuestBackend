563bd56388a36c49d479d53b563bbd1ef7eb82c6
timothyAgevi <timothyagevi@gmail.com>
2025-02-13 18:53:28 +0300

    updated the circle.ts

--- src/config/circles.ts
+++ src/config/circles.ts
@@ -1,30 +1,21 @@
 // config/circles.ts
-import { Sdk } from '@circles-sdk/sdk';
-import { BrowserProviderContractRunner } from '@circles-sdk/adapter-ethers';
-import { CirclesRpc, CirclesData } from '@circles-sdk/data';
-
-type ChainConfig = {
-  circlesRpcUrl: string;
-  v1HubAddress: string;
-  v2HubAddress: string;
-  migrationAddress: string;
-  nameRegistryAddress: string;
-  profileServiceUrl: string;
-  baseGroupMintPolicy: string;
-};
-
-const chainConfig: ChainConfig = {
-  circlesRpcUrl: 'https://static.94.138.251.148.clients.your-server.de/rpc/',
-  v1HubAddress: '0x29b9a7fbb8995b2423a71cc17cf9810798f6c543',
-  v2HubAddress: '0x3D61f0A272eC69d65F5CFF097212079aaFDe8267',
-  migrationAddress: '0x28141b6743c8569Ad8B20Ac09046Ba26F9Fb1c90',
-  nameRegistryAddress: '0x8D1BEBbf5b8DFCef0F7E2039e4106A76Cb66f968',
-  profileServiceUrl: 'https://static.94.138.251.148.clients.your-server.de/profiles/',
-  baseGroupMintPolicy: '0x79Cbc9C7077dF161b92a745345A6Ade3fC626A60',
-};
 
 export const initializeCirclesSdk = async () => {
   try {
+    const { BrowserProviderContractRunner } = await import('@circles-sdk/adapter-ethers');
+    const { CirclesRpc, CirclesData } = await import('@circles-sdk/data');
+    const { Sdk } = await import('@circles-sdk/sdk');
+    
+    const chainConfig = {
+      circlesRpcUrl: 'https://static.94.138.251.148.clients.your-server.de/rpc/',
+      v1HubAddress: '0x29b9a7fbb8995b2423a71cc17cf9810798f6c543',
+      v2HubAddress: '0x3D61f0A272eC69d65F5CFF097212079aaFDe8267',
+      migrationAddress: '0x28141b6743c8569Ad8B20Ac09046Ba26F9Fb1c90',
+      nameRegistryAddress: '0x8D1BEBbf5b8DFCef0F7E2039e4106A76Cb66f968',
+      profileServiceUrl: 'https://static.94.138.251.148.clients.your-server.de/profiles/',
+      baseGroupMintPolicy: '0x79Cbc9C7077dF161b92a745345A6Ade3fC626A60',
+    } as const;
+
     const adapter = new BrowserProviderContractRunner();
     await adapter.init();
 
