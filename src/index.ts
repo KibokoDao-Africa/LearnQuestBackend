b624e0ecc72658d22c7c50d710e87e041eccc5ff
timothyAgevi <timothyagevi@gmail.com>
2025-02-16 00:44:25 +0300

    add index route

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-// index.ts
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
@@ -13,6 +12,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 connectDB();
+
+// Index route
+app.get('/', (req, res) => {
+  res.send('Welcome to the LearnQuest API');
+});
 
 app.use('/api/users', userRoutes);
 app.use('/api/courses', courseRoutes);
